2ad0ddf5573453d51235a53d6bc338bde9ad4242
Naourr <princessmarielledepalubos2nd@gmail.com>
2025-07-04 12:42:15 +0800

    add loading screen

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const menu = document.querySelector('.menu-icon');
 const menulinks = document.querySelector('.menu-links');
 
+window.addEventListener("load", function() {
+  	document.querySelector(".loading").style.display = "none";
+});
 menu.addEventListener('click', function() {
     menulinks.classList.toggle('active');
 });

64c83bb54a45e4309cbe22b97988efd6f35f2ce1
Naourr <princessmarielledepalubos2nd@gmail.com>
2025-07-04 16:52:57 +0800

    nakaw parallax assets ayoo

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ window.addEventListener("load", function() {
 menu.addEventListener('click', function() {
     menulinks.classList.toggle('active');
 });
+
+const layers = document.querySelectorAll('.parallax-layer');
+
+document.addEventListener('mousemove', e => {
+    const x = (e.clientX / window.innerWidth - 0.5) * 2;
+    const y = (e.clientY / window.innerHeight - 0.5) * 2;
+
+    layers.forEach(layer => {
+        const depth = layer.dataset.depth;
+        const translateX = x * depth * 50;
+        const translateY = y * depth * 50;
+
+        layer.style.transform = `translate(${translateX}px, ${translateY}px)`;
+    });
+});
